11eb376aabb00bce8625bcc05ff834c8e5e38f1c
Andy Heggs <Andy@Heggs.net>
2024-08-30 12:55:08 +0100

    proxy change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/auth': 'http://127.0.0.1:8000',
+      '/auth': 'https://assetec-15d8b96cc456.herokuapp.com',
     },
   },
 });
